36f7929274ab8d71b3cb1aaca034877e370e4122
James Wade <github@jameshwade.com>
2024-02-07 22:21:18 -0500

    [fix] significant cleanup and updating from 2016 version

--- R/jsme.R
+++ R/jsme.R
@@ -1,55 +1,85 @@
-#' <Add Title>
+#' Create a JSME widget.
 #'
-#' <Add Description>
+#' JSME (Java Structure Editor for Molecular Editing) is a web app for
+#' editing molecular structures.  This function creates a GUI widget that
+#' can be included in an Rmarkdown document, which will contain the JSME
+#' app.  The app will allow users to interactively create or modify a molecular
+#' structure.  The widget will by default have a width and height of 650px,
+#' but these can be adjusted.
 #'
-#' @import htmlwidgets
-#'
+#' @param structure if character string, this is the initial structure
+#'   to be shown in the editor.  If left empty or absent, the editor will
+#'   start with a blank screen.  If numeric or logical or list or
+#'   data.frame, these will be converted to a character matrix and
+#'   passed to the editor as a SMILES.  See `?jsme::setMol` for more
+#'   information on the formats that JSME can accept.
+#' @param width,height integer or character string indicating the width,
+#'   height of the widget.  These will be passed to `createWidget` and
+#'   the units `px` are assumed (if not provided, the default is "650px").
+#' @return If successful, the function returns a list of class
+#'   "htmlwidget" containing the JSME widget, ready for inclusion in
+#'   an Rmarkdown document or Shiny app.
+#'   If an error occurs (e.g. because the structure is invalid),
+#'   the return value is printed to the console and NULL is returned.
 #' @export
-jsme <- function(structure='', width = '650px', height = '650px') {
+jsme <- function(structure = "", width = "650px", height = "650px") {
+  {
+    # forward options using x
+    x <- list(
+      structure = structure
+    )
 
-  # forward options using x
-  x = list(
-    structure = structure
-  )
-
-  # create widget
-  htmlwidgets::createWidget(
-    name = 'jsme',
-    x = x,
-    width = width,
-    height = height,
-    package = 'jsme'
-  )
+    # create widget
+    htmlwidgets::createWidget(
+      name = "jsme",
+      x = x,
+      width = width,
+      height = height,
+      package = "jsme"
+    )
+  }
 }
 
 smiles <- function(session, element, inputEl) {
-  session$sendCustomMessage(type = 'smiles',
-                            message = list(el=element, inputEl = inputEl))
+  session$sendCustomMessage(
+    type = "smiles",
+    message = list(el = element, inputEl = inputEl)
+  )
 }
 
 molFile <- function(session, element, inputEl) {
-  session$sendCustomMessage(type = 'molFile',
-                            message = list(el=element, inputEl = inputEl))
+  session$sendCustomMessage(
+    type = "molFile",
+    message = list(el = element, inputEl = inputEl)
+  )
 }
 
 jmeFile <- function(session, element, inputEl) {
-  session$sendCustomMessage(type = 'jmeFile',
-                            message = list(el=element, inputEl = inputEl))
+  session$sendCustomMessage(
+    type = "jmeFile",
+    message = list(el = element, inputEl = inputEl)
+  )
 }
 
 useMOL <- function(session, element, molFile) {
-  session$sendCustomMessage(type = 'useMOL',
-                            message = list(el=element, molFile = molFile))
+  session$sendCustomMessage(
+    type = "useMOL",
+    message = list(el = element, molFile = molFile)
+  )
 }
 
 useJME <- function(session, element, jmeFile) {
-  session$sendCustomMessage(type = 'useJME',
-                            message = list(el=element, jmeFile = jmeFile))
+  session$sendCustomMessage(
+    type = "useJME",
+    message = list(el = element, jmeFile = jmeFile)
+  )
 }
 
 resetEditor <- function(session, element) {
-  session$sendCustomMessage(type = 'resetEditor',
-                            message = list(el=element))
+  session$sendCustomMessage(
+    type = "resetEditor",
+    message = list(el = element)
+  )
 }
 
 #' Shiny bindings for jsme
@@ -57,7 +87,7 @@ resetEditor <- function(session, element) {
 #' Output and render functions for using jsme within Shiny
 #' applications and interactive Rmd documents.
 #'
-#' @param outputId output variable to read from
+#' @param outputId Output variable to read from.
 #' @param width,height Must be a valid CSS unit (like \code{'100\%'},
 #'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
 #'   string and have \code{'px'} appended.
@@ -69,13 +99,15 @@ resetEditor <- function(session, element) {
 #' @name jsme-shiny
 #'
 #' @export
-jsmeOutput <- function(outputId, width = '100px', height = '100px'){
-  htmlwidgets::shinyWidgetOutput(outputId, 'jsme', width, height, package = 'jsme')
+jsmeOutput <- function(outputId, width = "100px", height = "100px") {
+  htmlwidgets::shinyWidgetOutput(outputId, "jsme", width, height, package = "jsme")
 }
 
 #' @rdname jsme-shiny
 #' @export
 renderJsme <- function(expr, env = parent.frame(), quoted = FALSE) {
-  if (!quoted) { expr <- substitute(expr) } # force quoted
+  if (!quoted) {
+    expr <- substitute(expr)
+  } # force quoted
   htmlwidgets::shinyRenderWidget(expr, jsmeOutput, env, quoted = TRUE)
 }
